ba0e1ddcb1038c8d31763351b8001d50f6c2f974
Shoma Nishitateno <shoma416@gmail.com>
2025-09-15 22:10:44 +0900

    fix format

--- packages/mastra-agent/src/mastra/index.ts
+++ packages/mastra-agent/src/mastra/index.ts
@@ -1,4 +1,3 @@
-
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
@@ -6,14 +5,14 @@ import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow },
-  agents: { weatherAgent },
-  storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),
-  logger: new PinoLogger({
-    name: 'Mastra',
-    level: 'info',
-  }),
+	workflows: { weatherWorkflow },
+	agents: { weatherAgent },
+	storage: new LibSQLStore({
+		// stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+		url: ':memory:',
+	}),
+	logger: new PinoLogger({
+		name: 'Mastra',
+		level: 'info',
+	}),
 });
